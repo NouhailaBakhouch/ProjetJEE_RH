82167b50c7b7b64fec21e0f793b1893f2ae7f34b
NouhailaBakhouch <bakhouchnouhaila@gmail.com>
2024-04-20 20:50:12 +0200

    first commit

---
+++ src/main/java/springDataApp/ProjetJeeRhApplication.java
@@ -0,0 +1,13 @@
+package springDataApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetJeeRhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetJeeRhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springDataApp/ProjetJeeRhApplicationTests.java
@@ -0,0 +1,13 @@
+package springDataApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetJeeRhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
