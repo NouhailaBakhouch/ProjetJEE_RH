204c92ad7629807038e6ea68d96f53655b578ff6
med belhabes <82711509+medbelhabes@users.noreply.github.com>
2024-05-24 12:44:15 +0100

    Update ProjetJeeRhApplication.java

--- src/main/java/springDataApp/ProjetJeeRhApplication.java
+++ src/main/java/springDataApp/ProjetJeeRhApplication.java
@@ -2,8 +2,13 @@ package springDataApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
+@EntityScan(basePackages = "springDataApp.dao.entities")
+@EnableJpaRepositories(basePackages = "springDataApp.dao.repositories")
+@ComponentScan(basePackages = "springDataApp.service")
 public class ProjetJeeRhApplication {
 
 	public static void main(String[] args) {
