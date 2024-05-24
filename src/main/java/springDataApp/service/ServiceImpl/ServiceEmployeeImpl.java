4e5e3c4c3ce6e37820fb51868bbea9ff07289da2
NouhailaBakhouch <bakhouchnouhaila@gmail.com>
2024-05-24 12:38:52 +0100

    Package Service

---
+++ src/main/java/springDataApp/service/ServiceImpl/ServiceEmployeeImpl.java
@@ -0,0 +1,44 @@
+package springDataApp.service.ServiceImpl;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import springDataApp.dao.entities.Employee;
+import springDataApp.dao.reporistories.IEmpRepo;
+import springDataApp.service.IService.IServiceEmployee;
+
+@Service
+public class ServiceEmployeeImpl implements IServiceEmployee {
+
+    @Autowired
+    private IEmpRepo employeeRepository;
+
+    @Override
+    public void ajouterEmployee(Employee e) {
+    	employeeRepository.save(e);
+    }
+
+    @Override
+    public void supprimerEmployee(Integer id) {
+        employeeRepository.deleteById(id);
+    }
+
+    @Override
+    public void modifierEmployee(Employee e) {
+        employeeRepository.save(e);
+    }
+
+    @Override
+    public List<Employee> listerEmployee() {
+        return employeeRepository.findAll();
+    }
+
+    @Override
+    public Employee rechercherEmployee(Integer id) {
+        Optional<Employee> optional = employeeRepository.findById(id);
+        return optional.orElse(null);
+    }
+}
---
+++ src/main/java/springDataApp/service/ServiceImpl/package-info.java
@@ -0,0 +1,1 @@
+package springDataApp.service.ServiceImpl;
