d00f9e82a58b23df1fa14ea49e04d94d19861614
NouhailaBakhouch <bakhouchnouhaila@gmail.com>
2024-05-24 12:01:58 +0100

    Update Conge.java

--- src/main/java/springDataApp/dao/entities/Conge.java
+++ src/main/java/springDataApp/dao/entities/Conge.java
@@ -1,6 +1,28 @@
 package springDataApp.dao.entities;
 
-public class Conge {
-	private int cv;
+import java.util.Date;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Conge {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private Date dateDebut;
+    private Date dateFin;
+    private String type;
+
+    @ManyToOne
+    private Employee employee;
 }
